8a02b2b24e5f7647376f11763ba2051112dcb06a
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-03-11 13:13:09 +0100

    Client credential flow for access token.

---
+++ oauth/grants/client-credentials-token.js
@@ -0,0 +1,26 @@
+/**
+ * Fetches an access token using the Client Credentials grant type.
+ */
+
+require('dotenv').config({ path: __dirname + '/.env' })
+
+const { discover } = require('../lib/discovery')
+const { requireAccessToken, GrantType } = require('../lib/access-token')
+
+async function main() {
+    console.info()
+    console.info('Client Credentials Grant')
+    console.info('========================')
+    const oauthServerInfo = await discover(process.env.OAUTH2_DISCOVERY_URL)
+    await requireAccessToken({
+        grantType: GrantType.CLIENT_CREDENTIALS,
+        tokenEndpoint: oauthServerInfo['token_endpoint'],
+        clientId: process.env.OAUTH2_CLIENT_ID,
+        clientSecret: process.env.OAUTH2_CLIENT_SECRET,
+        scope: process.env.OAUTH2_SCOPE,
+    })
+}
+
+if (require.main === module) {
+    main().catch((error) => console.error(error))
+}
